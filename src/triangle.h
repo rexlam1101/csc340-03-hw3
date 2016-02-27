282e3af5e3854470902890f4ce8b65be15bd6170
Omar Shaikh <omarasifshaikh@hotmail.com>
2016-02-27 15:13:52 -0800

    initial

---
+++ src/triangle.h
@@ -0,0 +1,26 @@
+//
+// Created by Omar Shaikh on 2/23/16.
+//
+
+#ifndef HW3_TRIANGLE_H
+#define HW3_TRIANGLE_H
+/** Header file for the class Triangle. */
+
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include "triangle_interface.h"
+
+
+using namespace std;
+
+template<class T>
+class Triangle : public triangleInterface<T> {
+
+    /*TODO - Write your code here*/
+
+
+};
+
+
+#endif //HW3_TRIANGLE_H
